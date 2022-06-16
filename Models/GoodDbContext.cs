791aa360a7ddd75a8e68cd30a7f2a7325392161c
PostPunkAesthete <harry_ollI@abv.bg>
2022-06-16 13:05:59 +0300

    Initial commit

---
+++ Models/GoodDbContext.cs
@@ -0,0 +1,41 @@
+﻿using System.Data.Entity;
+
+namespace WarehouseGoods.Models
+{
+    public class GoodDbContext
+    {
+        public partial class GoodDBContext : DbContext
+        {
+            public GoodDBContext()
+            {
+            }
+
+            public GoodDBContext(DbContextOptions<GoodDBContext> options)
+                : base(options)
+            {
+            }
+
+            public virtual DbSet<Good> Food { get; set; }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                if (!optionsBuilder.IsConfigured)
+                {
+                    optionsBuilder.UseSqlServer("Server=.;Database=GoodDB;Integrated Security=true");
+                }
+            }
+
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                modelBuilder.Entity<Good>(entity =>
+                {
+                    entity.Property(e => e.Title).HasMaxLength(2500);
+                });
+
+                OnModelCreatingPartial(modelBuilder);
+            }
+
+            partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WarehouseGoods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            Console.WriteLine(String.Join(" ",queue));
+        }
+    }
+}
